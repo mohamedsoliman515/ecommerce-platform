14ed87e20a6bd2e758cf332ebc51b97c83656a67
muhammed-suliman <egypt.team.dev3@gmail.com>
2026-09-14 11:11:22 +0300

    Auth : created inngest as a Event-driven workflow

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,9 +3,16 @@ import { ENV } from "./config/env.js";
 import { connectDB } from "./config/db.js";
 import { clerkMiddleware } from '@clerk/express'
 import path from "path";
+import { serve } from "inngest/express";
+import { inngest ,functions} from "./config/inngest.js";
 
 const app = express();
+app.use(express.json());
 app.use(clerkMiddleware())
+app.use("/api/inngest", serve({ client: inngest ,functions}))
+
+
+
 
 app.get("/api/health", (req, res) => {
   res.status(200).json({ message: "Success" });
