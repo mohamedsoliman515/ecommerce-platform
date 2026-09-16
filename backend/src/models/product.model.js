85834f5b52cb4789e7c1c4c2d9bedb00e5b212bf
muhammed-suliman <egypt.team.dev3@gmail.com>
2026-09-16 11:02:59 +0300

    create product schema

---
+++ backend/src/models/product.model.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  imageUrl: {
+    type: String,
+    required: true,
+  },
+  stock: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+  averageRating:{
+    type:Number,
+    default: 0,
+    min: 0,
+    max: 5,
+  },
+  totalReviews:{
+    type:Number,
+    default: 0,
+  }
+},{
+  timestamps: true,
+});
+export const Product = mongoose.model("Product", productSchema);
+
