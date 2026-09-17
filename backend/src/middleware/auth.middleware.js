542b1abaee4f0e1f9f79799d998af79da43462b3
muhammed-suliman <egypt.team.dev3@gmail.com>
2026-09-17 09:46:30 +0300

    make Auth middleware

---
+++ backend/src/middleware/auth.middleware.js
@@ -0,0 +1,41 @@
+import { getAuth  } from "@clerk/express";
+import { User } from "../models/user.model.js";
+import { ENV } from "../config/env.js";
+
+
+
+// Authentication
+export const protectRoute = [
+    getAuth(),
+   async (req, res, next) => {
+   try {
+    const { userId } = req.auth;
+
+
+    if (!userId) {
+        return res.status(401).json({ message: "Unauthorized - invalid token" });
+    }
+    const user = await User.findOne({ clerkId: userId });
+    if (!user) {
+        return res.status(404).json({ message: "User not found" });
+    }
+    req.user = user;
+    next();
+
+   } catch (error) {
+    console.log(`Error in protectRoute middleware: ${error.message}`);
+    res.status(500).json({ message: "Internal server error" });
+    next(error);
+   }
+    }
+]
+// Authorization
+export const isAdmin = (req, res, next) => {
+    if(!req.user){
+        return res.status(401).json({ message: "Unauthorized - user not found" });
+    }
+    if (req.user.email !== ENV.ADMIN_EMAIL) {
+        return res.status(403).json({ message: "Forbidden - admin access only" });
+    }
+    next();
+}
