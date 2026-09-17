1b9e5c7bc166c008ddbe5fc3da0cb308b68332b0
muhammed-suliman <egypt.team.dev3@gmail.com>
2026-09-17 09:52:18 +0300

    add cart schema

---
+++ backend/src/models/cart.model.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+// user have many of carts or one cart
+
+const cartItemSchema = new mongoose.Schema({
+  productId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Product",
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+    min: 1,
+    default: 1,
+  },
+});
+
+const cartSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    clerkId: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+
+    items: [cartItemSchema],
+
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export const Cart = mongoose.model("Cart", cartSchema);
