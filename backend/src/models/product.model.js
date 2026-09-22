1bd819f1c1e4ef6f3cf2305939602973ccd3af8e
muhammed-suliman <egypt.team.dev3@gmail.com>
2026-09-22 17:18:33 +0300

    edit Schema

--- backend/src/models/product.model.js
+++ backend/src/models/product.model.js
@@ -1,44 +1,46 @@
 import mongoose from "mongoose";
 
-const productSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    imageUrl: {
+      type: String,
+      required: true,
+    },
+    stock: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    averageRating: {
+      type: Number,
+      default: 0,
+      min: 0,
+      max: 5,
+    },
+    totalReviews: {
+      type: Number,
+      default: 0,
+    },
   },
-  description: {
-    type: String,
-    required: true,
+  {
+    timestamps: true,
   },
-  price: {
-    type: Number,
-    required: true,
-    min: 0,
-  },
-  category: {
-    type: String,
-    required: true,
-  },
-  imageUrl: {
-    type: String,
-    required: true,
-  },
-  stock: {
-    type: Number,
-    required: true,
-    min: 0,
-  },
-  averageRating:{
-    type:Number,
-    default: 0,
-    min: 0,
-    max: 5,
-  },
-  totalReviews:{
-    type:Number,
-    default: 0,
-  }
-},{
-  timestamps: true,
-});
+);
 export const Product = mongoose.model("Product", productSchema);
-
